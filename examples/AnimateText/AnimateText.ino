612ce69451ed8024e17216da056daf4d09b4156d
Chris Boudacoff <d3v1c3nv11@users.noreply.github.com>
2019-11-01 08:50:46 +0200

    using timer isr

--- examples/AnimateText/AnimateText.ino
+++ examples/AnimateText/AnimateText.ino
@@ -1,24 +1,37 @@
 #include <SPI.h>
 #include "LedMatrix.h"
 
-
 #define NUMBER_OF_DEVICES 1
 #define CS_PIN 5
 
 LedMatrix ledMatrix = LedMatrix(NUMBER_OF_DEVICES, CS_PIN);
+int x = 0;
+
+void ICACHE_RAM_ATTR onTimerISR(){
+timer1_write(10000); //2000 us
+x++;
+if (x>30){
+x=0;
+  ledMatrix.clear();
+  ledMatrix.scrollTextLeft();
+  ledMatrix.drawText();
+  ESP.wdtFeed();
+}
+ledMatrix.commit();
+
+}
 
 void setup() {
+  Serial.begin(74600);
   ledMatrix.init();
   ledMatrix.setText("Testing Olimex LED 8x8 ");
+  //
+  timer1_attachInterrupt(onTimerISR);
+  timer1_enable(TIM_DIV16, TIM_EDGE, TIM_SINGLE);
+  timer1_write(10000); //2000 us
   }
 
 void loop() {
 
-  ledMatrix.clear();
-  ledMatrix.scrollTextLeft();
-  ledMatrix.drawText();
-  for (byte i = 0; i < 30; i++){
-  ledMatrix.commit();
-  delay(2);}
 
 }
